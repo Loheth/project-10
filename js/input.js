ed3cf290fa23dc61d5611feb8b9ca468041f6299
Loheth <lohethsenthil@gmail.com>
2026-02-18 12:30:56 -0500

    only dino outline left

---
+++ js/input.js
@@ -0,0 +1,222 @@
+function addKeyListeners() {
+	keypress.register_combo({
+		keys: "left",
+		on_keydown: function() {
+			if (MainHex && gameState !== 0) {
+				MainHex.rotate(1);
+			}
+		}
+	});
+
+	keypress.register_combo({
+		keys: "right",
+		on_keydown: function() {
+			if (MainHex && gameState !== 0){
+				MainHex.rotate(-1);
+			}
+		}
+	});
+		keypress.register_combo({
+		keys: "down",
+		on_keydown: function() {
+			var tempSpeed = settings.speedModifier;
+			if (MainHex && gameState !== 0){
+				if(settings.speedUpKeyHeld == false){
+					settings.speedUpKeyHeld = true;
+					window.rush *=4;
+				}
+			}
+		},
+		on_keyup:function(){
+			if (MainHex && gameState !== 0){
+				window.rush /=4;
+				settings.speedUpKeyHeld = false;
+			}
+		}	
+	});
+	
+	keypress.register_combo({
+		keys: "a",
+		on_keydown: function() {
+			if (MainHex && gameState !== 0) {
+				MainHex.rotate(1);
+			}
+		}
+	});
+
+	keypress.register_combo({
+		keys: "d",
+		on_keydown: function() {
+			if (MainHex && gameState !== 0){
+				MainHex.rotate(-1);
+			}
+		}
+	});
+	
+	keypress.register_combo({
+		keys: "s",
+		on_keydown: function() {
+			var tempSpeed = settings.speedModifier;
+			if (MainHex && gameState !== 0){
+				if(settings.speedUpKeyHeld == false){
+					settings.speedUpKeyHeld = true;
+					window.rush *=4;
+				}
+			}
+		},
+		on_keyup:function(){
+			if (MainHex && gameState !== 0){
+				window.rush /=4;
+				settings.speedUpKeyHeld = false;
+			}
+		}	
+	});
+	keypress.register_combo({
+		keys: "p",
+		on_keydown: function(){pause();}
+	});
+
+	keypress.register_combo({
+		keys: "space",
+		on_keydown: function(){pause();}
+	});
+
+	keypress.register_combo({
+		keys: "q",
+		on_keydown: function() {
+			if (devMode) toggleDevTools();
+		}
+	});
+
+	keypress.register_combo({
+		keys: "enter",
+		on_keydown: function() {
+			if (gameState==1 || importing == 1) {
+				init(1);
+			}
+			if (gameState == 2) {
+				init();
+				$("#gameoverscreen").fadeOut();
+			}
+			if (gameState===0) {
+				resumeGame();
+			}
+		}
+	});
+
+	$("#pauseBtn").on('touchstart mousedown', function() {
+		if (gameState != 1 && gameState != -1) {
+			return;
+		}
+
+		if ($('#helpScreen').is(":visible")) {
+			$('#helpScreen').fadeOut(150, "linear");
+		}
+		pause();
+		return false;
+	});
+
+	$("#colorBlindBtn").on('touchstart mousedown', function() {
+	window.colors = ["#ff3366", "#ff9500", "#00d4ff", "#00ff88"];
+	window.hexColorsToTintedColors = {
+		"#ff3366": "rgb(255,130,160)",
+		"#ff9500": "rgb(255,190,100)",
+		"#00d4ff": "rgb(100,230,255)",
+		"#00ff88": "rgb(100,255,180)"
+	};
+	window.rgbToHex = {
+		"rgb(255,51,102)": "#ff3366",
+		"rgb(255,149,0)": "#ff9500",
+		"rgb(0,212,255)": "#00d4ff",
+		"rgb(0,255,136)": "#00ff88"
+	};
+	window.rgbColorsToTintedColors = {
+		"rgb(255,51,102)": "rgb(255,130,160)",
+		"rgb(255,149,0)": "rgb(255,190,100)",
+		"rgb(0,212,255)": "rgb(100,230,255)",
+		"rgb(0,255,136)": "rgb(100,255,180)"
+	};
+	});
+
+
+	if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+			$("#restart").on('touchstart', function() {
+			init();
+			canRestart = false;
+			$("#gameoverscreen").fadeOut();
+		});
+
+	}
+	else {
+		$("#restart").on('mousedown', function() {
+			init();
+			canRestart = false;
+			$("#gameoverscreen").fadeOut();
+		});
+
+	}
+	if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+			$("#restartBtn").on('touchstart', function() {
+			init(1);
+			canRestart = false;
+			$("#gameoverscreen").fadeOut();
+		});
+
+	}
+	else {
+		$("#restartBtn").on('mousedown', function() {
+			init(1);
+			canRestart = false;
+			$("#gameoverscreen").fadeOut();
+		});
+
+
+	}
+
+}
+function inside (point, vs) {
+	var x = point[0], y = point[1];
+	
+	var inside = false;
+	for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+		var xi = vs[i][0], yi = vs[i][1];
+		var xj = vs[j][0], yj = vs[j][1];
+		
+		var intersect = ((yi > y) != (yj > y))
+			&& (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+		if (intersect) inside = !inside;
+	}
+	
+	return inside;
+};
+
+function handleClickTap(x,y) {
+	if (x < 120 && y < 83 && $('.helpText').is(':visible')) {
+		showHelp();
+		return;
+	}
+	var radius = settings.hexWidth ;
+	var halfRadius = radius/2;
+	var triHeight = radius *(Math.sqrt(3)/2);
+	var Vertexes =[
+		[radius,0],
+		[halfRadius,-triHeight],
+		[-halfRadius,-triHeight],
+		[-radius,0],
+		[-halfRadius,triHeight],
+		[halfRadius,triHeight]];
+	Vertexes = Vertexes.map(function(coord){ 
+		return [coord[0] + trueCanvas.width/2, coord[1] + trueCanvas.height/2]});
+
+	if (!MainHex || gameState === 0 || gameState==-1) {
+		return;
+	}
+
+	if (x < window.innerWidth/2) {
+		MainHex.rotate(1);
+	}
+	if (x > window.innerWidth/2) {
+		MainHex.rotate(-1);
+	}
+}
+
